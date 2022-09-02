eb3c3db9bdbbdddc92af14c391a0e4acf92e6c27
Kaique <kaique@neurony.com.br>
2022-09-01 21:11:52 -0300

    Adiciona tratativa de erros

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 import { router } from "./routes";
 
 const app = express();
 app.use(express.json());
 
 app.use(router);
+
+//MiddleWare
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    //Se for uma instancia do tipo erro
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  });
+});
 
 app.listen(3333, () => console.log("Servidor online"));
