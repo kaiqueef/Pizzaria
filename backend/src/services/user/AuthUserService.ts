2fc5240a042bb541cb5f920b1b52f6ec4ddf91cf
Kaique <kaique@neurony.com.br>
2022-09-02 17:27:16 -0300

    Cria rota de autenticação de usuário

---
+++ backend/src/services/user/AuthUserService.ts
@@ -0,0 +1,32 @@
+import prismaClient from "../../prisma";
+import { compare } from "bcryptjs";
+
+interface AuthRequest {
+  email: string;
+  password: string;
+}
+
+class AuthUserService {
+  async execute({ email, password }: AuthRequest) {
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    const userDoesntExist = !user;
+    if (userDoesntExist) {
+      throw new Error("User/password incorrect");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("User/password incorrect");
+    }
+
+    return { ok: true };
+  }
+}
+
+export { AuthUserService };
