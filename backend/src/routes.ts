0b37f25ee64caa8aca70fb4397e36443da6477eb
Kaique <kaique@neurony.com.br>
2022-09-02 14:09:12 -0300

    Configura estrutura de controllers e services

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,8 @@
-import { Router, Request, Response } from "express";
-
+import { Router } from "express";
+import { CreateUserController } from "./controllers/user/createUserController";
 const router = Router();
 
-router.get("/teste", (req: Request, res: Response) => {
-    // return res.json({ ok: true})
-  throw new Error("Erro ao fazer essa requisição");
-});
+// ROTAS USER
+router.post("/users", new CreateUserController().handle);
 
 export { router };
