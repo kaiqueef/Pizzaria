cfa52a7a72505ceea20c97c898f96ad0074aabe4
Kaique <kaique@neurony.com.br>
2022-09-05 11:32:08 -0300

    Cria rota para cadastro de categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 
 const router = Router();
 
@@ -12,5 +13,12 @@ router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+
+// ROTAS CATEGORY
+router.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle
+);
 
 export { router };
---
+++ backend/src/services/category/CreateCategoryService.ts
@@ -0,0 +1,32 @@
+import prismaClient from "../../prisma";
+
+interface CategoryRequest {
+  name: string;
+}
+class CreateCategoryService {
+  async execute({ name }: CategoryRequest) {
+    if (name === "") {
+      throw new Error("Invalid Name");
+    }
+
+    const categoryAlreadyExists = await prismaClient.category.findFirst({
+      where: {
+        name: name,
+      },
+    });
+    if (categoryAlreadyExists)
+      throw new Error("This category already exists in our database");
+
+    const category = await prismaClient.category.create({
+      data: {
+        name: name,
+      },
+      select: {
+        id: true,
+        name: true,
+      },
+    });
+    return category;
+  }
+}
+export default CreateCategoryService;
