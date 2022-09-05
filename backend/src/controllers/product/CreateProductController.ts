09cd3336c4c697d70da228e48995a889177e28e9
Kaique <kaique@neurony.com.br>
2022-09-05 13:48:27 -0300

    Configura envio de imagens

--- backend/src/controllers/product/CreateProductController.ts
+++ backend/src/controllers/product/CreateProductController.ts
@@ -5,19 +5,22 @@ class CreateProductController {
   async handle(req: Request, res: Response) {
     const { name, price, description, category_id } = req.body;
 
-    let banner = "";
-
     const createProductService = new CreateProductService();
 
-    const product = await createProductService.execute({
-      name,
-      price,
-      description,
-      banner,
-      category_id,
-    });
-
-    return res.json(product);
+    if (!req.file) {
+      throw new Error("Error uploading file");
+    } else {
+      const { originalname, filename } = req.file;
+      console.log(filename);
+      const product = await createProductService.execute({
+        name,
+        price,
+        description,
+        banner: "",
+        category_id,
+      });
+      return res.json(product);
+    }
   }
 }
 
