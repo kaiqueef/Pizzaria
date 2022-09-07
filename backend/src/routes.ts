d70f7058ff954c8d131c7016fd9317eb1ba495c5
Kaique <kaique@neurony.com.br>
2022-09-07 20:05:25 -0300

    Adiciona rota para adicionar Items ao pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -11,6 +11,7 @@ import uploadConfig from "./config/multer";
 import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
 import { CreateOrderController } from "./controllers/order/CreateOrderController";
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
+import { AddItemController } from "./controllers/order/AddItemController";
 
 const router = Router();
 
@@ -49,5 +50,7 @@ router.get(
 // ROTAS ORDER
 router.post("/order", isAuthenticated, new CreateOrderController().handle);
 router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
+
+router.post("/order/add", isAuthenticated, new AddItemController().handle);
 
 export { router };
